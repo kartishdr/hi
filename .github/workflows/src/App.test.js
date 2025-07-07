499ca7a9e18c7b935ab08a29e2687fdee4b4fa52
kartishdr <46895633+kartishdr@users.noreply.github.com>
2025-07-07 12:48:12 +0530

    Delete .github/workflows/src directory

--- .github/workflows/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from './App';
-
-test('renders Hello from React!', () => {
-  render(<App />);
-  const heading = screen.getByText(/Hello from React!/i);
-  expect(heading).toBeInTheDocument();
-});
